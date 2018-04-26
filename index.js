ec33b43db28075de211e98bef2ce65af6d4ead52
Sergiu Enasoaie <sergiu.enasoaie@tss-yonder.com>
2018-04-26 11:12:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,149 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var engine = require('./public/game')
+var gameStarted = false;
+var gameInterval, updateInterval;
+
+function genName() {
+  var names = [
+    'Babel',
+    'Ionic',
+    'Sass',
+    'Jade',
+    'AngularJS',
+    'React',
+    'Redux',
+    'jQuery',
+    'LeSS',
+    'PhantomJS',
+    'Git',
+  ];
+  return names[Math.floor(Math.random() * names.length)];
+}
+
+// TODO: extract below
+function gameLoop() {
+  // move everyone around
+  Object.keys(engine.players).forEach((playerId) => {
+    let player = engine.players[playerId];
+    engine.movePlayer(playerId);
+
+    if (engine.players[playerId].score == engine.maxPlayers) {
+      gameStarted = false;
+      io.emit('gameFinished');
+      
+      // clearInterval(gameInterval);
+      // clearInterval(updateInterval);
+    }
+  })
+}
+
+// ----------------------------------------
+// Main server code
+// ----------------------------------------
+
+// serve css and js
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+function emitUpdates() {
+  // tell everyone what's up
+  io.emit('gameStateUpdate', { players: engine.players, doubloon: engine.doubloon });
+}
+
+io.on('connection', function(socket){
+  console.log('User connected: ', socket.id);
+
+  // set socket listeners
+  socket.on('playerWantsToJoin', function () {
+
+    // don't add any more players
+    if (gameStarted) {
+      socket.emit('enoughPlayers');
+      return;
+    }
+
+    // get open position
+    var posX = 0
+    var posY = 0
+    while (!engine.isValidPosition({ x: posX, y: posY }, socket.id)) {
+      posX = Math.floor(Math.random() * Number(engine.gameSizeX) - 100) + 10
+      posY = Math.floor(Math.random() * Number(engine.gameSizeY) - 100) + 10
+    }
+
+    // add player to engine.players obj
+    engine.players[socket.id] = {
+      accel: {
+        x: 0,
+        y: 0
+      },
+      x: posX,
+      y: posY,
+      colour: engine.stringToColour(socket.id),
+      score: 0,
+      name: genName()
+    }
+
+    // send the curent number of players so we show exactly what happens
+    io.emit('currentPlayers', Object.keys(engine.players).length);
+
+    // start game if this is the first player
+    if (Object.keys(engine.players).length == engine.maxPlayers) {
+      gameStarted = true;
+    }
+  });
+
+  socket.on('start', function() {
+    io.emit('startGame');
+    engine.shuffleDoubloon();
+    gameInterval = setInterval(gameLoop, 25);
+    updateInterval = setInterval(emitUpdates, 40);
+  });
+
+  // set socket listeners
+  socket.on('disconnect', function () {
+    if (engine.players[socket.id]) {
+      delete engine.players[socket.id];
+
+      // send the curent number of players so we show exactly what happens
+      io.emit('currentPlayers', Object.keys(engine.players).length);
+
+      gameStarted = false;
+    }
+   
+    // end game if there are no engine.players left
+    if (gameStarted) return;
+    if (Object.keys(engine.players).length > 0) {
+      io.emit('gameStateUpdate', { players: engine.players, doubloon: engine.doubloon });
+    } else {
+      clearInterval(gameInterval)
+      clearInterval(updateInterval)
+    }
+  });
+
+  socket.on('up', function (msg) {
+    engine.accelPlayer(socket.id, 0, -1)
+  });
+
+  socket.on('down', function (msg) {
+    engine.accelPlayer(socket.id, 0, 1)
+  })
+
+  socket.on('left', function (msg) {
+    engine.accelPlayer(socket.id, -1, 0)
+  });
+
+  socket.on('right', function (msg) {
+    engine.accelPlayer(socket.id, 1, 0)
+  })
+});
+
+http.listen(process.env.PORT || 8080, function(){
+  console.log('listening on *:8080', process.env.PORT);
+});
---
+++ public/client.js
@@ -0,0 +1,212 @@
+
+    var socket = io();
+    var canvas = document.getElementById('game');
+    var join = document.getElementById('join');
+    var ctx = canvas.getContext('2d');
+    var localDirection; // used to display accel direction
+    var gameLoopVar;
+    var enoughPlayers = false;
+
+    // register service worker
+    if ('serviceWorker' in navigator) {
+      console.log("Will the service worker register?");
+      navigator.serviceWorker.register('service-worker.js')
+        .then(function (reg) {
+          console.log("Yes, it did.");
+        }).catch(function (err) {
+          console.log("No it didn't. This happened:", err)
+        });
+    }
+
+    // add event that will add the player on the list 
+    join.addEventListener('click', function(event){
+      enoughPlayers = false;
+      document.getElementById('waitingContainer').style.display = 'block';
+      document.getElementById('joinContainer').style.display = 'none';
+      socket.emit('playerWantsToJoin');
+      if (document.getElementById('maxPlayer')) document.getElementById('maxPlayer').innerHTML = maxPlayers;
+    });
+
+    socket.on('currentPlayers', (count) => {
+      if (document.getElementById('connected')) document.getElementById('connected').innerHTML = count;
+    });
+    
+    socket.on('startGame', function(){
+      if (enoughPlayers) return;
+      document.getElementById('waitingContainer').style.display = 'none';
+      document.getElementById('gameContainer').style.display = 'block';
+      gameLoopVar = setInterval(gameLoop, 25);
+      requestAnimationFrame(drawGame);
+    });
+
+    socket.on('enoughPlayers', function() {
+      enoughPlayers = true;
+      document.getElementById('waitingContainer').querySelector('h3').innerText = 'The limit of maximum players was reached!';
+      document.getElementById('waitingContainer').querySelector('span').innerText = 'Refresh and try to join again, maybe a spot is opened.';
+    });
+
+    socket.on('gameStateUpdate', updateGameState);
+    socket.on('gameFinished', () => {
+      if (enoughPlayers) {
+        document.getElementById('waitingContainer').style.display = 'none';
+        document.getElementById('gameStats').innerHTML = 'The game has finished, maybe next time!';
+        return;
+      }
+      console.log(players);
+      console.log(socket.id);
+      document.getElementById('gameContainer').style.display = 'none';
+      var theWinner = Object.keys(players).filter(playerId => playerId == socket.id && players[playerId].score == 3);
+      console.log(theWinner);
+      if (theWinner.length === 1) {
+        document.getElementById('gameStats').innerHTML = 'You\'ve won a shot, come in front :)';
+      } else {
+        document.getElementById('gameStats').innerHTML = 'Game Over. You\'ve LOST :(';
+      }
+    });
+
+    var playerImages = {};
+    function drawPlayers(players) {
+      // draw players
+      // the game world is 500x500, but we're downscaling 5x to smooth accel out
+      Object.keys(players).forEach((playerId) => {
+        let player = players[playerId];
+        var direction;
+
+        if (!playerImages.hasOwnProperty(playerId)) {
+          var image = new Image(20, 20);   // using optional size for image
+          // load an image of intrinsic size 300x227 in CSS pixels
+          if (playerId == socket.id) {
+            image.src = `public/images/shot-sergiu.png`;
+          } else {
+            image.src = `public/images/black.png`;
+          }
+
+          playerImages[playerId] = image;
+          image.onload = function () {
+            ctx.drawImage(playerImages[playerId], player.x / 5, player.y / 5, playerSize / 5, playerSize / 5);
+          }
+        } else {
+          ctx.drawImage(playerImages[playerId], player.x / 5, player.y / 5, playerSize / 5, playerSize / 5);
+        }
+        
+        // ctx.fillStyle = player.colour;
+        // ctx.fillRect(player.x/5, player.y/5, playerSize/5, playerSize/5);
+
+        if (playerId == socket.id) {
+          direction = localDirection
+        } else {
+          direction = player.direction
+        }
+        // draw accel direction for current player based on local variable
+        // the idea here is to give players instant feedback when they hit a key
+        // to mask the server lag
+        ctx.fillStyle = 'black';
+        let accelWidth = 3
+        switch(direction) {
+          case 'up':
+            ctx.fillRect(player.x/5, player.y/5 - accelWidth, playerSize/5, accelWidth);
+            break
+          case 'down':
+            ctx.fillRect(player.x/5, player.y/5  + playerSize/5, playerSize/5, accelWidth);
+            break
+          case 'left':
+            ctx.fillRect(player.x/5 - accelWidth, player.y/5, accelWidth, playerSize/5);
+            break
+          case 'right':
+            ctx.fillRect(player.x/5 + playerSize/5, player.y/5, accelWidth, playerSize/5);
+        }
+      })
+    }
+
+    var image = new Image(20, 20);   // using optional size for image
+    // load an image of intrinsic size 300x227 in CSS pixels
+    image.src = `public/images/mouth.png`;
+    function updateGameState(gameState) {
+      // update local state to match state on server
+      players = gameState.players
+      doubloon = gameState.doubloon
+
+      // draw stuff
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      Object.keys(players).forEach((playerId) => {
+        if (playerId == socket.id) {
+          let player = players[playerId]
+          document.getElementById('name').innerHTML = player.name;
+          document.getElementById('score').innerHTML = player.score;
+        }
+      });
+      
+
+      // draw doubloon
+      ctx.beginPath();
+
+      // ctx.drawImage(image, doubloon.x / 5, doubloon.y / 5, doubloonSize, doubloonSize);
+      ctx.drawImage(image, (doubloon.x + doubloonSize / 2) / 5, (doubloon.y + doubloonSize / 2) / 5, doubloonSize / 2, doubloonSize / 2);
+      // ctx.arc((doubloon.x + doubloonSize/2)/5, (doubloon.y + doubloonSize/2)/5, doubloonSize/5, 0, 2 * Math.PI, false);
+      // ctx.fillStyle = 'gold';
+      // ctx.fill();
+      // ctx.lineWidth = 2;
+      // ctx.strokeStyle = '#003300';
+      // ctx.stroke();
+
+      drawPlayers(players)
+    }
+
+    // key handling
+    $('html').keydown(function(e) {
+      if (e.key == "ArrowDown") {
+        socket.emit('down', players);
+        accelPlayer(socket.id, 0, 1)
+        localDirection = 'down'
+      } else if (e.key == "ArrowUp") {
+        socket.emit('up', players);
+        accelPlayer(socket.id, 0, -1)
+        localDirection = 'up'
+      } else if (e.key == "ArrowLeft") {
+        socket.emit('left', players);
+        accelPlayer(socket.id, -1, 0)
+        localDirection = 'left'
+      } else if (e.key == "ArrowRight") {
+        socket.emit('right', players);
+        accelPlayer(socket.id, 1, 0)
+        localDirection = 'right'
+      }
+    })
+
+    $('.navigation').on('click', function () {
+      if ($(this).data('id') == "down") {
+        socket.emit('down', players);
+        accelPlayer(socket.id, 0, 1)
+        localDirection = 'down'
+      } else if ($(this).data('id') == "up") {
+        socket.emit('up', players);
+        accelPlayer(socket.id, 0, -1)
+        localDirection = 'up'
+      } else if ($(this).data('id') == "left") {
+        socket.emit('left', players);
+        accelPlayer(socket.id, -1, 0)
+        localDirection = 'left'
+      } else if ($(this).data('id') == "right") {
+        socket.emit('right', players);
+        accelPlayer(socket.id, 1, 0)
+        localDirection = 'right'
+      }
+    });
+
+    function gameLoop() {
+      // update game
+      updateGameState({ players: players, doubloon: doubloon})
+      // move everyone around
+      Object.keys(players).forEach((playerId) => {
+        let player = players[playerId]
+        movePlayer(playerId)
+      })
+    }
+
+    function drawGame() {
+      // draw stuff
+      drawPlayers(players)
+      requestAnimationFrame(drawGame)
+    }
+
